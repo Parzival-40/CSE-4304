41906cd80517affbf8c4d9bd09e763e5e189c441
Md Abdullahil Kafi <abdullahilkafi@iut-dhaka.edu>
2021-09-21 23:52:06 +0600

    Fixed Lazy Tree

--- Lab 7/190041224_T03L07_2B.cpp
+++ Lab 7/190041224_T03L07_2B.cpp
@@ -4,52 +4,60 @@ using namespace std;
 
 class SegTree {
 private:
-	vector<int> a;
+	vector<pair<int, int>> a;
 	int n;
-	void build(vector<int>& b, int v, int l, int r) {
+	static int inline range(const int& l, const int& r) {
+		return r - l + 1;
+	}
+	void build(const vector<int>& b, const int& v, const int& l, const int& r) {
 		if (l == r) {
-			a[v] = b[l];
+			a[v].first = b[l];
+			a[v].second = 0;
 		} else {
 			int m = (l + r) / 2;
 			build(b, v * 2, l, m);
 			build(b, v * 2 + 1, m + 1, r);
-			a[v] = 0;
+			a[v].first = a[v * 2].first + a[v * 2 + 1].first;
 		}
 	}
-	void update(int v, int l, int r, int i, int j, int add) {
+	void update(const int& v, const int& l, const int& r, const int& i, const int& j, const int& add) {
 		if (i > j) {
 			return;
 		}
 		if (l == i && r == j) {
-			a[v] += add;
+			a[v].first += range(l, r) * add;
+			a[v].second += add;
+			return;
 		} else {
 			int m = (l + r) / 2;
 			update(v * 2, l, m, i, min(m, j), add);
 			update(v * 2 + 1, m + 1, r, max(m + 1, i), j, add);
+			a[v].first = a[v * 2].first + a[v * 2 + 1].first + range(l, r) * a[v].second;
 		}
 	}
-	int get(int v, int l, int r, int pos) {
-		if (l == r) {
-			return a[v];
+	int query(const int& v, const int& l, const int& r, const int& i, const int& j, const int& c = 0) {
+		if (i > j) {
+			return 0;
+		}
+		if (l == i && r == j) {
+			return a[v].first + c * range(l, r);
 		}
 		int m = (l + r) / 2;
-		if (pos > m)
-			return a[v] + get(v * 2 + 1, m + 1, r, pos);
-		else
-			return a[v] + get(v * 2, l, m, pos);
+		return query(v * 2, l, m, i, min(m, j), c + a[v].second) +
+			   query(v * 2 + 1, m + 1, r, max(m + 1, i), j, c + a[v].second);
 	}
 
 public:
 	SegTree(vector<int>& b)
 	  : n(b.size()) {
-		a.resize(4 * n, 0);
+		a.resize(4 * n, make_pair(0, 0));
 		build(b, 1, 0, n - 1);
 	}
-	void update(int l, int r, int add) {
+	void update(const int& l, const int& r, const int& add) {
 		update(1, 1, n, l, r, add);
 	}
-	int get(int pos) {
-		return get(1, 1, n, pos);
+	int query(const int& l, const int& r) {
+		return query(1, 1, n, l, r);
 	}
 };
 
@@ -61,12 +69,14 @@ int main() {
 		cin >> i;
 	SegTree b(a);
 	while (q--) {
-		int l, r, add;
-		cin >> l >> r >> add;
-		b.update(l, r, add);
-		for (int i = 1; i <= n; i++) {
-			cout << b.get(i) << ' ';
+		int c, l, r;
+		cin >> c >> l >> r;
+		if (c == 1) {
+			int add;
+			cin >> add;
+			b.update(l, r, add);
+		} else if (c == 2) {
+			cout << b.query(l, r) << endl;
 		}
-		cout << endl;
 	}
 }
